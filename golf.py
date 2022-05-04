5f6c75ed88a53fe1d614600ea3c815200948d905
Peter Lawson <lawsonpd@pm.me>
2022-05-03 21:16:51 -0500

    Add Hand and Deck classes and replace some functions with class methods.

--- golf.py
+++ golf.py
@@ -1,35 +1,36 @@
 import random
 import itertools
 
+class Deck:
+    def __init__(self):
+        self.draw_pile = None
+        self.discard_pile = None
+
+    def deal_hands(self, hands):
+        pass
+
+class Hand:
+    def __init__(self):
+        self.cards = {} # Key, value: card, known/unknown
+
+    def __gt__(self, value):
+        # May use for comparisons in max/allmax
+        pass
+
+    def swap_card(self, new, old):
+        pass
+
+    def hand_score(self):
+        return sum([v for v, s in self.cards])
+
 def golf(hands):
-    return allmax(hands, key=hand_score)
+    # return allmax(hands, key=hand_score)
+    pass
 
 def allmax(hands):
     pass
 
-def hand_score(hand):
-    pass
-
-def hand(k=4):
-    "Returns a hand of k cards."
-    pass
-
-def deal():
-    pass
-
-def score_hand(hand):
-    pass
-
-def swap_card(hand, old_card, new_card):
-    pass
-
-def draw_pile():
-    pass
-
-def discard_pile():
-    pass
-
-player_turn = itertools.cycle(range(num_players))
+# player_turn = itertools.cycle(range(num_players))
 # Could be something like the following
 # (which doesn't work because it returns an iterator, which is not what we want):
 # def player_turn(n=4):
